e526128f1820b1aceb9a0f4ec44278182bf3149e
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2024-04-02 17:10:45 +0530

    full screen

--- src/Page/Slide.tsx
+++ src/Page/Slide.tsx
@@ -4,7 +4,7 @@ import "./Slide.css"; // Import CSS file for panel styling
 const Slide: React.FC = () => {
   // State to control panel visibility
   const [showPanel, setShowPanel] = useState<boolean>(false);
-
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
   // Function to toggle panel visibility
   const togglePanel = () => {
     setShowPanel(!showPanel);
@@ -12,14 +12,27 @@ const Slide: React.FC = () => {
 
   // Function to close panel
   const closePanel = () => {
+    //setIsFullScreen(false)
     setShowPanel(false);
+   
   };
-
+  const handleFullScreen = () => {
+    // setIsFullScreen(!isFullScreen);
+    const element = document.getElementById("pan")
+    const isNotFullScreen= document.fullscreenElement;
+    if(isNotFullScreen){
+        document.exitFullscreen()
+        setIsFullScreen(false)
+    }else {
+        element?.requestFullscreen()
+        setIsFullScreen(true)
+    }
+  };
   return (
     <div className="panel-container">
       <button onClick={togglePanel}>Show Panel</button>
       {/* Render the panel conditionally based on state */}
-      <div className={`panel ${showPanel ? "active" : ""}`}>
+      <div id ="pan" className={`panel ${showPanel ? "active" : ""}`}>
         {/* Cancel icon to close panel */}
         <span className="cancel-icon" onClick={closePanel}>
           &#10006;
@@ -30,6 +43,9 @@ const Slide: React.FC = () => {
         <div className="test">
         <p>This is the content of the panel.</p>
         </div>
+        <button onClick={handleFullScreen}>
+        {isFullScreen ? "Exit Full Screen" : "Go Full Screen"}
+      </button>
       </div>
     </div>
   );
